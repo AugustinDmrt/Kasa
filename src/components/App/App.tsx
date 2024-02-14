bca0d215fe61d34d5b691f41602bc309c838a356
Augustin Damourette <a.damourette@gauthier.corp>
2024-02-14 16:23:09 +0100

    Configuration de l'espage de travail

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import reactLogo from "../../../src/assets/react.svg";
+import "./App.css";
+import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
--- src/components/BrowserRouter.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot, ReactRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
-const root: ReactRoot = createRoot(document.getElementById("root"));
-
-root.render(
-  <BrowserRouter>{/* The rest of your app goes here */}</BrowserRouter>
-);
