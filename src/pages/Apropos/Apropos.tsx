57fd68a61a619fe34766f7a461439e19ef98abcc
AugustinDmrt <55685689+AugustinDmrt@users.noreply.github.com>
2024-04-06 10:37:29 +0200

    ajout des lorem ipsum page apropos

--- src/pages/Apropos/Apropos.tsx
+++ src/pages/Apropos/Apropos.tsx
@@ -8,10 +8,30 @@ export default function Apropos() {
     <div id="apropos-page">
       <Banner img={BannerImgApropos} text="" />
       <div className="apropos-collapse">
-        <Collapse title="Fiabilité" data={[]} />
-        <Collapse title="Respect" data={[]} />
-        <Collapse title="Service" data={[]} />
-        <Collapse title="Sécurité" data={[]} />
+        <Collapse
+          title="Fiabilité"
+          data={[
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+          ]}
+        />
+        <Collapse
+          title="Respect"
+          data={[
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+          ]}
+        />
+        <Collapse
+          title="Service"
+          data={[
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+          ]}
+        />
+        <Collapse
+          title="Sécurité"
+          data={[
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+          ]}
+        />
       </div>
     </div>
   );
